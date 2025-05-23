edb77672e4a4aec795bc54c66edd4c56501d7cfc
csdarren <2001dhall@gmail.com>
2025-05-23 16:18:57 -0400

    Fixed clangd config to use custom esp32 clangd only when sdkconfig file found. Removed unused keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,19 +6,17 @@
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, silent = true })
 -- Remap Space + p to paste from the clipboard instead of vim buffer
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true, silent = true })
--- Remap Space + p + v to execute command :Ex in command mode
-vim.keymap.set({ "n", "v" }, "<leader>pv", ":Ex<CR>", { noremap = true, silent = true })
 -- Remap Space + I to execute command :Inspect in command mode
 vim.keymap.set({ "n" }, "<leader>I", ":Inspect<CR>", { noremap = true, silent = true })
 -- Move cursor to original position after undoing or redoing
 vim.keymap.set("n", "U", function()
-  local curpos = vim.api.nvim_win_get_cursor(0)
-  vim.cmd("redo")
-  vim.api.nvim_win_set_cursor(0, curpos)
+    local curpos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd("redo")
+    vim.api.nvim_win_set_cursor(0, curpos)
 end, { desc = "Redo without moving cursor" })
 
 vim.keymap.set("n", "u", function()
-  local curpos = vim.api.nvim_win_get_cursor(0)
-  vim.cmd("undo")
-  vim.api.nvim_win_set_cursor(0, curpos)
+    local curpos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd("undo")
+    vim.api.nvim_win_set_cursor(0, curpos)
 end, { desc = "Undo without moving cursor" })
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,17 @@
---local esp32 = require("esp32")
+local esp32 = require("esp32")
 return {
     {
         "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                --   clangd = esp32.lsp_config(),
-                ruff = { autostart = false },
-            },
-        },
+        opts = function(_, opts)
+            opts.servers = opts.servers or {}
+
+            -- only use esp32 clangd if in esp32 project
+            if esp32.find_esp_clangd() and vim.fn.filereadable("sdkconfig") == 1 then
+                opts.servers.clangd = esp32.lsp_config()
+            end
+
+            opts.servers.ruff = { autostart = false }
+        end,
     },
     {
         "mason-org/mason.nvim",
