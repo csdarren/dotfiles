ba6d4f3cc76e8de0963fd9ddd0b731428fd38d4c
csdarren <2001dhall@gmail.com>
2025-06-24 17:01:34 -0400

    Removed ESPIDF config, added platformio plugin.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,10 +4,13 @@
 
 -- Remap Space + y to copy to the clipboard instead of vim buffer
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, silent = true })
+
 -- Remap Space + p to paste from the clipboard instead of vim buffer
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true, silent = true })
+
 -- Remap Space + I to execute command :Inspect in command mode
 vim.keymap.set({ "n" }, "<leader>I", ":Inspect<CR>", { noremap = true, silent = true })
+
 -- Move cursor to original position after undoing or redoing
 vim.keymap.set("n", "U", function()
     local curpos = vim.api.nvim_win_get_cursor(0)
@@ -20,3 +23,18 @@ vim.keymap.set("n", "u", function()
     vim.cmd("undo")
     vim.api.nvim_win_set_cursor(0, curpos)
 end, { desc = "Undo without moving cursor" })
+
+-- Platform IO
+vim.keymap.set({ "n", "v" }, "<leader>jt", ":Piocmdf<CR>", { noremap = true, silent = true, desc = "Terminal" })
+vim.keymap.set({ "n", "v" }, "<leader>ji", ":Pioinit<CR>", { noremap = true, silent = true, desc = "Init Project" })
+vim.keymap.set({ "n", "v" }, "<leader>jr", ":Piorun<CR>", { noremap = true, silent = true, desc = "Run Project" })
+vim.keymap.set({ "n", "v" }, "<leader>jd", ":Piodebug<CR>", { noremap = true, silent = true, desc = "Debugger" })
+vim.keymap.set(
+    { "n", "v" },
+    "<leader>jl",
+    ":PioTermList<CR>",
+    { noremap = true, silent = true, desc = "Terminal List" }
+)
+vim.keymap.set({ "n", "v" }, "<leader>jm", ":Piomon<CR>", { noremap = true, silent = true, desc = "Serial Monitor" })
+vim.keymap.set({ "n", "v" }, "<leader>j", ":Pio<CR>", { noremap = true, silent = true, desc = "Refresh LSP Support?" })
+-- vim.keymap.set({ "n", "v" }, "<leader>j", ":Pio<CR>", { noremap = true, silent = true, desc = "" })
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,18 +1,10 @@
-local esp32 = require("esp32")
 return {
     {
         "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-            opts.servers = opts.servers or {}
-
-            -- only use esp32 clangd if in esp32 project
-            if esp32.find_esp_clangd() and vim.fn.filereadable("sdkconfig") == 1 then
-                opts.servers.clangd = esp32.lsp_config()
-            end
-
-            opts.servers.ruff = { autostart = false }
-            opts.inlay_hints = { enabled = false }
-        end,
+        servers = {
+            ruff = { autostart = false },
+            inlay_hints = { enabled = false },
+        },
     },
     {
         "mason-org/mason.nvim",
