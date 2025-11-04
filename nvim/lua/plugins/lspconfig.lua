7ecc45ae903c577323a12a0a2f19e7f771140d29
csdarren <2001dhall@gmail.com>
2025-11-04 14:03:40 -0500

    Updates, Fixed ESP32 Config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,18 @@
 return {
     {
         "neovim/nvim-lspconfig",
-        servers = {
-            ruff = { autostart = false },
-            inlay_hints = { enabled = false },
-        },
+        opts = function(_, opts)
+            local esp32 = require("esp32")
+
+            -- ensure opts.servers exists
+            opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+                ruff = { autostart = false },
+                inlay_hints = { enabled = false },
+                clangd = esp32.lsp_config(),
+            })
+
+            return opts
+        end,
     },
     {
         "mason-org/mason.nvim",
@@ -16,4 +24,20 @@ return {
             },
         },
     },
+    -- vim.lsp.config("ccls", {
+    --     init_options = {
+    --         cache = {
+    --             directory = ".ccls-cache",
+    --         },
+    --         compilationDatabaseDirectory = "build",
+    --         index = {
+    --             threads = 2,
+    --             trackDependencies = true,
+    --         },
+    --         clang = {
+    --             excludeArgs = { "-frounding-math" },
+    --         },
+    --     },
+    -- }),
+    -- vim.lsp.enable("ccls"),
 }
